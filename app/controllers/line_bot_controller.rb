c01ddce5595099a8d925b304a5e71d1b556e1368
miloneko <satohannn@gmail.com>
2022-08-26 12:41:34 +0900

    feat: 環境変数の設定 #7

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,4 +2,13 @@ class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
   def callback
   end
+
+  private
+ 
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = Rails.application.credentials.dig(:line, :channel_secret)
+      config.channel_token = Rails.application.credentials.dig(:line, :channel_token)
+    }
+  end
 end
