6dc351e9e33b402b312e2a9f8e8733186847abbb
miloneko <satohannn@gmail.com>
2022-08-27 23:27:04 +0900

    fix: インデント修正

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -8,26 +8,26 @@ class LineBotController < ApplicationController
     end
     events = client.parse_events_from(body)
     events.each do |event|
-        case event
-        when Line::Bot::Event::Message
-          case event.type
-          when Line::Bot::Event::MessageType::Text
-            message = {
-                type: 'text',
-                text: event.message['text']
-              }
-              client.reply_message(event['replyToken'], message)
-          end
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+            }
+            client.reply_message(event['replyToken'], message)
         end
       end
+    end
       head :ok
   end
   private
- 
+
   def client
     @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = Rails.application.credentials.dig(:line, :channel_secret)
-    config.channel_token = Rails.application.credentials.dig(:line, :channel_token)
-   }
+      config.channel_secret = Rails.application.credentials.dig(:line, :channel_secret)
+      config.channel_token = Rails.application.credentials.dig(:line, :channel_token)
+    }
   end
 end
