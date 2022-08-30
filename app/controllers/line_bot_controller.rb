570e4d2c8a7496d284b13871e52fc5e7537da3ff
miloneko <satohannn@gmail.com>
2022-08-30 23:30:12 +0900

    fix: 構文修正 #19

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,9 +3,8 @@ class LineBotController < ApplicationController
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      return head :bad_request
-    end
+    return head :bad_request unless client.validate_signature(body, signature)
+
     events = client.parse_events_from(body)
     events.each do |event|
       case event
@@ -15,8 +14,8 @@ class LineBotController < ApplicationController
           message = {
             type: 'text',
             text: event.message['text']
-            }
-          if event.message['text'] =~ /移動手段（かかりつけ医がいる方）/
+          }
+          if /移動手段（かかりつけ医がいる方）/.match?(event.message['text'])
             message = {
               "type": 'text',
               "text": "移動手段を以下の手順で検討してみてください
@@ -31,34 +30,34 @@ class LineBotController < ApplicationController
 救急車をご検討ください。救急車を呼ぶか迷った場合はこちらを参照ください。
 参考記事のurl"
             }
-          client.reply_message(event['replyToken'], message)
+            client.reply_message(event['replyToken'], message)
           end
-          if event.message['text'] =~ /COCOAの通知があり相談したい方/
+          if /COCOAの通知があり相談したい方/.match?(event.message['text'])
             message = {
               "type": 'text',
               "text": "COCOA通知がきて相談したい方は【東京都発熱相談センターCOCOA専用ダイヤル】で相談できます。
 [電話番号]000-0000-0000"
             }
-          client.reply_message(event['replyToken'], message)
+            client.reply_message(event['replyToken'], message)
           end
-          if event.message['text'] =~ /コロナについてご質問がある方/
+          if /コロナについてご質問がある方/.match?(event.message['text'])
             message = {
               "type": 'text',
               "text": "新型コロナに対して不安に思う方は【新型コロナ・オミクロン株コールセンター】で相談ができます。
 [電話番号]0570-550571"
             }
-          client.reply_message(event['replyToken'], message)
+            client.reply_message(event['replyToken'], message)
           end
-          if event.message['text'] =~ /診療可能な病院を探す/
+          if /診療可能な病院を探す/.match?(event.message['text'])
             message = {
               "type": 'text',
               "text": "【該当の番号を、入力してメッセージで送信してください】
 1.近くの診療可能な医療機関を検索
 2.東京都発熱相談センターへ相談する"
             }
-          client.reply_message(event['replyToken'], message)
+            client.reply_message(event['replyToken'], message)
           end
-          if event.message['text'] =~ /1/
+          if /1/.match?(event.message['text'])
             message = {
               "type": 'text',
               "text": "近くの病院を検索中
@@ -67,26 +66,27 @@ class LineBotController < ApplicationController
 4.発熱者が児童である
 5.該当しない"
             }
-          client.reply_message(event['replyToken'], message)
+            client.reply_message(event['replyToken'], message)
           end
-          if event.message['text'] =~ /5/
+          if /5/.match?(event.message['text'])
             message = {
               "type": 'text',
-              "text": "診療可能な病院が○件見つかりました"
+              "text": '診療可能な病院が○件見つかりました'
             }
-          client.reply_message(event['replyToken'], message)
+            client.reply_message(event['replyToken'], message)
           end
         end
       end
     end
-      head :ok
+    head :ok
   end
+
   private
 
   def client
-    @client ||= Line::Bot::Client.new { |config|
+    @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = Rails.application.credentials.dig(:line, :channel_secret)
       config.channel_token = Rails.application.credentials.dig(:line, :channel_token)
-    }
+    end
   end
 end
