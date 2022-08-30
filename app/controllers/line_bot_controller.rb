8a0ad12127ff899d7d03f5f6aae5773c3f63e25b
miloneko <satohannn@gmail.com>
2022-08-30 23:23:33 +0900

    feat: 特定の言葉に返答する機能 #19

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -16,7 +16,66 @@ class LineBotController < ApplicationController
             type: 'text',
             text: event.message['text']
             }
-            client.reply_message(event['replyToken'], message)
+          if event.message['text'] =~ /移動手段（かかりつけ医がいる方）/
+            message = {
+              "type": 'text',
+              "text": "移動手段を以下の手順で検討してみてください
+1.自家用車がある場合
+自家用車でのご移動をお願いします
+2.自家用車がない場合
+発熱タクシー（有料）をご検討ください
+発熱タクシーのURL
+3.公共交通機関を利用の場合
+もし公共交通機関を利用する場合は感染対策を徹底して人と距離をとり移動すること
+4.上記の移動手段が困難な場合
+救急車をご検討ください。救急車を呼ぶか迷った場合はこちらを参照ください。
+参考記事のurl"
+            }
+          client.reply_message(event['replyToken'], message)
+          end
+          if event.message['text'] =~ /COCOAの通知があり相談したい方/
+            message = {
+              "type": 'text',
+              "text": "COCOA通知がきて相談したい方は【東京都発熱相談センターCOCOA専用ダイヤル】で相談できます。
+[電話番号]000-0000-0000"
+            }
+          client.reply_message(event['replyToken'], message)
+          end
+          if event.message['text'] =~ /コロナについてご質問がある方/
+            message = {
+              "type": 'text',
+              "text": "新型コロナに対して不安に思う方は【新型コロナ・オミクロン株コールセンター】で相談ができます。
+[電話番号]0570-550571"
+            }
+          client.reply_message(event['replyToken'], message)
+          end
+          if event.message['text'] =~ /診療可能な病院を探す/
+            message = {
+              "type": 'text',
+              "text": "【該当の番号を、入力してメッセージで送信してください】
+1.近くの診療可能な医療機関を検索
+2.東京都発熱相談センターへ相談する"
+            }
+          client.reply_message(event['replyToken'], message)
+          end
+          if event.message['text'] =~ /1/
+            message = {
+              "type": 'text',
+              "text": "近くの病院を検索中
+以下のいずれかのケースに該当しますか？【該当の番号を、入力してメッセージで送信してください】
+3.妊娠している
+4.発熱者が児童である
+5.該当しない"
+            }
+          client.reply_message(event['replyToken'], message)
+          end
+          if event.message['text'] =~ /5/
+            message = {
+              "type": 'text',
+              "text": "診療可能な病院が○件見つかりました"
+            }
+          client.reply_message(event['replyToken'], message)
+          end
         end
       end
     end
