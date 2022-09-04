0328aecba51110bbfc2556bfe4db7234bfaba77d
yusukedayo <tsunemi0217@icloud.com>
2022-09-04 14:34:04 +0900

    Add: 病院情報が登録されたら自動で緯度経度を追加するように設定 #23

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,6 @@
 class Hospital < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
   has_many :business_hours
   has_one :target_group
   has_one :inspection_type
