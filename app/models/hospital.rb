5abdcba33cf309d3ebdc6eed13fa86e5ec67be04
yusukedayo <tsunemi0217@icloud.com>
2022-09-04 14:58:13 +0900

    Add: ローカル環境でrails db:migrateを実行#23

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: hospitals
+#
+#  id                  :bigint           not null, primary key
+#  address             :string           not null
+#  holiday_support     :integer          default(0), not null
+#  latitude            :float
+#  longitude           :float
+#  name                :string           not null
+#  notification        :text
+#  phone_number        :string           not null
+#  primary_care_doctor :integer          default(0), not null
+#  url                 :text             not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
 class Hospital < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
