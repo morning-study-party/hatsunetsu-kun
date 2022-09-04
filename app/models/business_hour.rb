81e401964372898a5ebe4bc056237b4788b5be00
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-09-04 16:17:20 +0900

    chore: マイグレーション実行時にannotateがスキーマ情報を各モデルファイルに記述

--- app/models/business_hour.rb
+++ app/models/business_hour.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: business_hours
+#
+#  id          :bigint           not null, primary key
+#  close_at    :time             not null
+#  day_of_week :integer          default(0), not null
+#  meridian    :integer          default(0), not null
+#  open_at     :time             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  hospital_id :bigint           not null
+#
+# Indexes
+#
+#  index_business_hours_on_hospital_id  (hospital_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (hospital_id => hospitals.id)
+#
 class BusinessHour < ApplicationRecord
   belongs_to :hospital
 end
--- app/models/inspection_type.rb
+++ app/models/inspection_type.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: inspection_types
+#
+#  id                       :bigint           not null, primary key
+#  pcr                      :integer          default(0), not null
+#  qualitative_antigen_test :integer          default(0), not null
+#  quantitative_inspection  :integer          default(0), not null
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  hospital_id              :bigint
+#
+# Indexes
+#
+#  index_inspection_types_on_hospital_id  (hospital_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (hospital_id => hospitals.id)
+#
 class InspectionType < ApplicationRecord
 	belongs_to :hospital
 end
