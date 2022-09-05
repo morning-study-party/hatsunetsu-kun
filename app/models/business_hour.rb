7990540be0f94a2bea753302f238fea45afb4b83
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-09-05 23:07:09 +0900

    feat:各モデルにenumの設定を記載 #22

--- app/models/business_hour.rb
+++ app/models/business_hour.rb
@@ -21,4 +21,6 @@
 #
 class BusinessHour < ApplicationRecord
   belongs_to :hospital
+  enum meridian: { am: 0, pm: 1}
+  enum day_of_week: { Monday: 0, Tuesday: 1, Wednesday: 2, Thursday: 3, Friday: 4, Saturday: 5, Sunday: 6}
 end
