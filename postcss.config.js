fe3e44b3d6945db03e57b25c154bbfa8c86b45ad
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-09-09 16:05:48 +0900

    chore: postcss.config.jsに読み込むライブラリの設定を追加 #33

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    require('tailwindcss')('./app/javascript/packs/tailwind.config.js'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
