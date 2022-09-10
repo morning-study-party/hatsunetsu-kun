377a2567797cadd18b8183a9b27d7afd423bfc49
miloneko <satohannn@gmail.com>
2022-09-10 17:03:34 +0900

    fix: 改行コード使用

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -19,24 +19,24 @@ class LineBotController < ApplicationController
             message = {
               "type": 'text',
               "text": "移動手段を以下の手順で検討してみてください
-1.自家用車がある場合
-自家用車でのご移動をお願いします
-2.自家用車がない場合
-発熱タクシー（有料）をご検討ください
-発熱タクシーのURL
-3.公共交通機関を利用の場合
-もし公共交通機関を利用する場合は感染対策を徹底して人と距離をとり移動すること
-4.上記の移動手段が困難な場合
-救急車をご検討ください。救急車を呼ぶか迷った場合はこちらを参照ください。
-参考記事のurl"
+                      \n1.自家用車がある場合
+                      \n自家用車でのご移動をお願いします
+                      \n2.自家用車がない場合
+                      \n発熱タクシー（有料）をご検討ください
+                      \n発熱タクシーのURL
+                      \n3.公共交通機関を利用の場合
+                      \nもし公共交通機関を利用する場合は感染対策を徹底して人と距離をとり移動すること
+                      \n4.上記の移動手段が困難な場合
+                      \n救急車をご検討ください。救急車を呼ぶか迷った場合はこちらを参照ください。
+                      \n参考記事のurl"
             }
             client.reply_message(event['replyToken'], message)
           end
           if /コロナまたはCOCOA通知について相談したい方/.match?(event.message['text'])
             message = {
               "type": 'text',
-              "text": "COCOA通知がきて相談したい方は【東京都発熱相談センターCOCOA専用ダイヤル】で相談できます。
-[電話番号]000-0000-0000"
+              "text": "COCOA通知がきて相談したい方は【東京都発熱相談センターCOCOA専用ダイヤル】で相談できます。\n[電話番号]000-0000-0000
+                      \n新型コロナに対して不安に思う方は【新型コロナ・オミクロン株コールセンター】で相談ができます。\n[電話番号]0570-550571"
             }
             client.reply_message(event['replyToken'], message)
           end
@@ -44,8 +44,7 @@ class LineBotController < ApplicationController
             message = {
               "type": 'text',
               "text": "【該当の番号を、入力してメッセージで送信してください】
-1.近くの診療可能な医療機関を検索
-2.東京都発熱相談センターへ相談する"
+                      \n1.近くの診療可能な医療機関を検索\n2.東京都発熱相談センターへ相談する"
             }
             client.reply_message(event['replyToken'], message)
           end
@@ -53,10 +52,7 @@ class LineBotController < ApplicationController
             message = {
               "type": 'text',
               "text": "近くの病院を検索中
-以下のいずれかのケースに該当しますか？【該当の番号を、入力してメッセージで送信してください】
-3.妊娠している
-4.発熱者が児童である
-5.該当しない"
+                      \n以下のいずれかのケースに該当しますか？【該当の番号を、入力してメッセージで送信してください】\n 3.妊娠している\n 4.発熱者が児童である\n 5.該当しない"
             }
             client.reply_message(event['replyToken'], message)
           end
