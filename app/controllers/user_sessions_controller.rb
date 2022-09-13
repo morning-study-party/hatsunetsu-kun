72eb888f2e6573e66f5e28a77910a8b582df5569
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-09-14 00:45:35 +0900

    feat: ログイン、ログアウト処理のコントローラーのアクションの中身を追記 #5

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,8 +2,17 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to rails_admin_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    logout
+    redirect_to root_path
   end
 end
