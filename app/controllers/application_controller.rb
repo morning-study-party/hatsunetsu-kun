5672479f41550f9fbf1765f8be58d077a7b4b167
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-09-14 00:53:37 +0900

    feat: require_loginのコールバックを設定し、トップページとログインに必要なアクションはskipする設定を追加 #5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+
+  def not_authenticated
+    flash[:info] = 'ログインしてください'
+    redirect_to main_app.login_path
+  end
 end
