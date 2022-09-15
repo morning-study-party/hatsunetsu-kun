83007744e4d59082ae9f49fd732868cbad8b709e
yamashita-a420 <ayanonagai420@gmail.com>
2022-09-15 23:04:12 +0900

    Update: 開発環境データを10件分から全件へ差し替え #38

--- db/fixtures/development/04_business_hours.rb
+++ db/fixtures/development/04_business_hours.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-business_hours_csv = CSV.readlines("db/fixtures/development/business_hours_10.csv")
+business_hours_csv = CSV.readlines("db/fixtures/development/business_hours.csv")
 business_hours_csv.shift
 business_hours_csv.each do |row|
   BusinessHour.create(open_at: row[1], close_at: row[2], meridian: row[3].to_i, day_of_week: row[4].to_i, hospital_id: row[5].to_i)
