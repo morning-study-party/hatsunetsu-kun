1ff92ac2638abb9942e7feb813af3cb7756f6f21
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-09-18 10:01:34 +0900

    fix: 最新のmainをpull

--- db/fixtures/development/01_hospitals.rb
+++ db/fixtures/development/01_hospitals.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-hospitals_csv = CSV.readlines("db/fixtures/development/hospitals_10.csv")
+hospitals_csv = CSV.readlines("db/fixtures/development/hospitals.csv")
 hospitals_csv.shift
 hospitals_csv.each do |row|
   Hospital.create(name: row[1], phone_number: row[2], url: row[3], notification: row[4], primary_care_doctor: row[5].to_i, address: row[6], holiday_support: row[7].to_i)
--- db/fixtures/development/03_inspection_types.rb
+++ db/fixtures/development/03_inspection_types.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-inspection_types_csv = CSV.readlines("db/fixtures/development/inspection_types_10.csv")
+inspection_types_csv = CSV.readlines("db/fixtures/development/inspection_types.csv")
 inspection_types_csv.shift
 inspection_types_csv.each do |row|
   InspectionType.create(pcr: row[1].to_i, qualitative_antigen_test: row[2].to_i, quantitative_inspection: row[3].to_i, hospital_id: row[4].to_i)
