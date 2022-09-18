393db607398f7747869d419389bcd5dae2de03fd
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-09-18 11:12:42 +0900

    fix: herokuデプロイ時のアセットの読み込み関連のエラーの対応

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../../assets/stylesheets/application.css'
+import '../../assets/stylesheets/application.scss'
 
 Rails.start()
 Turbolinks.start()
